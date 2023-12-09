70eb13ca18feca7fc07fd1b73f17d8c4cd4078f6
DanielVierh <daniel.vierheilig89@yahoo.de>
2023-12-09 22:56:23 +0100

    load and save func

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,41 +1,41 @@
-const modal_edit = document.getElementById("modal_edit");
-const modal_exercise = document.getElementById("modal_exercise");
-const modal_settings = document.getElementById("modal_settings");
+// const modal_edit = document.getElementById("modal_edit");
+// const modal_exercise = document.getElementById("modal_exercise");
+// const modal_settings = document.getElementById("modal_settings");
 
-const btn_home = document.getElementById("btn_home");
-const btn_open_edit = document.getElementById("btn_open_edit");
-const btn_settings = document.getElementById("btn_settings");
+// const btn_home = document.getElementById("btn_home");
+// const btn_open_edit = document.getElementById("btn_open_edit");
+// const btn_settings = document.getElementById("btn_settings");
 
-const modal_close_btn = document.querySelectorAll('.modal_close_btn');
+// const modal_close_btn = document.querySelectorAll('.modal_close_btn');
 
-const modal_list = [modal_edit, modal_exercise, modal_settings];
+// const modal_list = [modal_edit, modal_exercise, modal_settings];
 
 
-btn_open_edit.addEventListener('click', ()=> {
-    open_modal(modal_edit);
-});
+// btn_open_edit.addEventListener('click', ()=> {
+//     open_modal(modal_edit);
+// });
 
-btn_settings.addEventListener('click', ()=> {
-    open_modal(modal_settings);
-});
+// btn_settings.addEventListener('click', ()=> {
+//     open_modal(modal_settings);
+// });
 
-btn_home.addEventListener('click', () => {
-    close_all_modals();
-});
+// btn_home.addEventListener('click', () => {
+//     close_all_modals();
+// });
 
-function open_modal(modal) {
-    close_all_modals();
-    modal.classList.add('active');
-}
+// function open_modal(modal) {
+//     close_all_modals();
+//     modal.classList.add('active');
+// }
 
-function close_all_modals() {
-    for (let i = 0; i < modal_list.length; i++) {
-        modal_list[i].classList.remove('active');
-    }
-}
+// function close_all_modals() {
+//     for (let i = 0; i < modal_list.length; i++) {
+//         modal_list[i].classList.remove('active');
+//     }
+// }
 
-modal_close_btn.forEach((c_btn)=> {
-    c_btn.addEventListener('click', ()=> {
-        close_all_modals();
-    })
-})
+// modal_close_btn.forEach((c_btn)=> {
+//     c_btn.addEventListener('click', ()=> {
+//         close_all_modals();
+//     })
+// })
--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,22 @@
 //* ANCHOR - Elemente
 
 const exercise_container = document.getElementById('exercise_container');
+const btn_add = document.getElementById('btn_add');
+const btn_saveExercise = document.getElementById('btn_saveExercise');
+const modal_edit = document.getElementById("modal_edit");
+const modal_exercise = document.getElementById("modal_exercise");
+const modal_settings = document.getElementById("modal_settings");
+const btn_home = document.getElementById("btn_home");
+const btn_open_edit = document.getElementById("btn_open_edit");
+const btn_settings = document.getElementById("btn_settings");
+const modal_close_btn = document.querySelectorAll('.modal_close_btn');
+
+
+const modal_list = [modal_edit, modal_exercise, modal_settings];
 
 
 //* ANCHOR -  Variablen
+let training_running = false;
 
 
 //*  Saveobj
@@ -24,11 +37,51 @@ let save_Object = {
 // * ANCHOR - Init
 /////////////////////////////////////
 window.onload = ()=> {
-    loadAndShow();
-    
-    setTimeout(() => {
-        render_exercises();
-    }, 500);
+    load_local_storage();
+}
+
+
+//########################################
+//* ANCHOR - Load Local Storage
+//########################################
+function load_local_storage() {
+    if (localStorage.getItem('stored_fitness_saveobj') != '') {
+        try {
+            save_Object = JSON.parse(
+                localStorage.getItem('stored_fitness_saveobj'),
+            );
+            training_running = save_Object.training_is_running;
+            setTimeout(() => {
+                //* Render func
+                render_exercises();
+            }, 500);
+            try {
+                weeklylist = save_Object.saved_weekly_list;
+                if(weeklylist === undefined) {
+                    weeklylist = [];
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+            save_Object = {
+                training_is_running: false,
+                exercises: [],
+                trainings: [],
+                current_training: [],
+                training_place_filter: '',
+            };
+        }
+    }
+}
+
+
+//########################################
+//* ANCHOR - Save to local Storage
+//########################################
+function save_into_storage() {
+    localStorage.setItem('stored_fitness_saveobj', JSON.stringify(save_Object));
 }
 
 
@@ -90,18 +143,6 @@ function rnd_id() {
     return key;
 }
 
-/////////////////////////////////////
-//*DELETE test objects
-/////////////////////////////////////
-function loadAndShow() {
-    const liegestuetze = new Exercise(rnd_id(), 'Liegestütze', '-', 3, 10, '-', '-')
-    const chesspress = new Exercise(rnd_id(), 'chestpress', 45, 3, 12, '31', 'Stufe 6', 'Brust', 'Fitnessstudio')
-
-    save_Object.exercises.push(chesspress);
-    save_Object.exercises.push(liegestuetze);
-
-    console.log('Saveobject', save_Object);
-}
 
 /////////////////////////////////////
 //* ANCHOR - Render exercises
@@ -120,14 +161,104 @@ function render_exercises() {
 }
 
 /////////////////////////////////////
-//* ANCHOR -
+//* ANCHOR - Save new Exercise
 /////////////////////////////////////
+btn_saveExercise.addEventListener('click', ()=> {
+    let inpExercise_Name = document.getElementById('inpExercise_Name');
+    let inpExercise_Weight = document.getElementById('inpExercise_Weight');
+    let inpExercise_Sets = document.getElementById('inpExercise_Sets');
+    let inpExercise_Repeats = document.getElementById('inpExercise_Repeats');
+    let inpExercise_number = document.getElementById('inpExercise_number');
+    let inpExercise_seatSettings = document.getElementById('inpExercise_seatSettings');
+    let muscle_select = document.getElementById('muscle_select');
+    let training_Area = document.getElementById('training_Area');
+    if(inpExercise_Name.value === '') {
+        inpExercise_Name.value = '-'
+    }
+    if(inpExercise_Weight.value === '') {
+        inpExercise_Weight.value = 0;
+    }
+    if(inpExercise_Sets.value === '') {
+        inpExercise_Sets.value = 0;
+    }
+    if(inpExercise_Repeats.value === '') {
+        inpExercise_Repeats.value = 0; 
+    }
+    if(inpExercise_number.value === '') {
+        inpExercise_number.value = '-'
+    }
+    if(inpExercise_seatSettings.value === '') {
+        inpExercise_seatSettings.value = '-'
+    }
+    if(muscle_select.value === '') {
+        muscle_select.value = '-';
+    }
+    if(training_Area.value === '') {
+        training_Area.value = '-';
+    }
+
+    const newExercise = new Exercise(rnd_id(), 
+                            inpExercise_Name.value, 
+                            inpExercise_Weight.value, 
+                            inpExercise_Sets.value, 
+                            inpExercise_Repeats.value, 
+                            inpExercise_number.value, 
+                            inpExercise_seatSettings.value, 
+                            muscle_select.value, 
+                            training_Area.value, 
+                            0
+    );
+    save_Object.exercises.push(newExercise);
+    save_into_storage();
+    location.reload();
+})
+
 
 /////////////////////////////////////
 //* ANCHOR -
 /////////////////////////////////////
-
+/////////////////////////////////////
+//* ANCHOR -
+/////////////////////////////////////
 /////////////////////////////////////
 //* ANCHOR -
 /////////////////////////////////////
 
+
+
+/////////////////////////////////////
+//* ANCHOR - Modals
+/////////////////////////////////////
+
+btn_open_edit.addEventListener('click', ()=> {
+    open_modal(modal_edit);
+});
+
+btn_settings.addEventListener('click', ()=> {
+    open_modal(modal_settings);
+});
+
+btn_add.addEventListener('click', ()=> {
+    open_modal(modal_edit);
+});
+
+btn_home.addEventListener('click', () => {
+    close_all_modals();
+});
+
+function open_modal(modal) {
+    close_all_modals();
+    modal.classList.add('active');
+}
+
+function close_all_modals() {
+    for (let i = 0; i < modal_list.length; i++) {
+        modal_list[i].classList.remove('active');
+    }
+}
+
+modal_close_btn.forEach((c_btn)=> {
+    c_btn.addEventListener('click', ()=> {
+        close_all_modals();
+    })
+})
