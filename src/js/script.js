29929937f6dafa2ba61ba10b5f035107eddd5c37
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-01-24 23:08:52 +0100

    add solved icon

--- src/js/script.js
+++ src/js/script.js
@@ -386,7 +386,9 @@ function render_exercises(exerc_array, label) {
             let exerciseName = exerc_array[i].name;
             try {
                 const currentSet = save_Object.current_training[`${indexOfExercise(exerc_array[i], save_Object.current_training)}`].solved_sets;
-                exerciseName = `${exerc_array[i].name} (${currentSet}/${exerc_array[i].sets})`;
+                exerciseName = `${exerc_array[i].name} (${currentSet}/${exerc_array[i].sets}) <span style="margin: 0 10px"> </span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+                <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
+              </svg>`;
                 if (currentSet >= exerc_array[i].sets) {
                     exercisebtn.classList.add('solved');
                 } else {
