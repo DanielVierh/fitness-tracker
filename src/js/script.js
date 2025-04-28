530973f577a57b6a6a2fd50a6674add240c86231
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-04-28 22:06:04 +0200

    change table order for solved training

--- src/js/script.js
+++ src/js/script.js
@@ -613,11 +613,6 @@ btn_trackSport.addEventListener('click', () => {
             save_into_storage();
 
             //TODO -  replace alert
-            // Modal.close_all_modals();
-            // setTimeout(() => {
-            //     const message = new Message('Ein neues Training wurde gestartet.', 'success', 5000);
-            //     message.showMessage();
-            // }, 500);
             const message = new Message('Training gestartet', 'Ein neues Training wurde gestartet. 1 Satz wurde hinzugefügt', 'success', 2500);
             message.showMessage();
             // alert("Ein weiterer Satz wurde hinzugefügt");
@@ -993,59 +988,77 @@ function createTable(title, data, only_exercise) {
     const table = document.createElement("table");
     const header = document.createElement("tr");
     const nameHeaderCell = document.createElement("th");
-    const weightHeaderCell = document.createElement("th");
-    const repsHeaderCell = document.createElement("th");
     const setsHeaderCell = document.createElement("th");
+    const repsHeaderCell = document.createElement("th");
+    const weightHeaderCell = document.createElement("th");
+    const totalWeightHeaderCell = document.createElement("th");
     const muscleHeaderCell = document.createElement("th");
     nameHeaderCell.appendChild(document.createTextNode("Übung"));
-    weightHeaderCell.appendChild(document.createTextNode("Gew"));
-    repsHeaderCell.appendChild(document.createTextNode("Wdh"));
     setsHeaderCell.appendChild(document.createTextNode("Sätze"));
+    repsHeaderCell.appendChild(document.createTextNode("Wdh"));
+    weightHeaderCell.appendChild(document.createTextNode("Gew"));
+    totalWeightHeaderCell.appendChild(document.createTextNode("Sum"));
     muscleHeaderCell.appendChild(document.createTextNode("Muskel"));
     header.appendChild(nameHeaderCell);
-    header.appendChild(weightHeaderCell);
-    header.appendChild(repsHeaderCell);
     header.appendChild(setsHeaderCell);
+    header.appendChild(repsHeaderCell);
+    header.appendChild(weightHeaderCell);
+    header.appendChild(totalWeightHeaderCell);
     header.appendChild(muscleHeaderCell);
     table.appendChild(header);
+
     for (let i = 0; i < data.length; i++) {
         const row = document.createElement("tr");
         const nameCell = document.createElement("td");
-        const weightCell = document.createElement("td");
-        const repsCell = document.createElement("td");
         const setsCell = document.createElement("td");
+        const repsCell = document.createElement("td");
+        const weightCell = document.createElement("td");
+        const totalWeightCell = document.createElement("td");
         const muscleCell = document.createElement("td");
+        const totalWeight = data[i].weight * data[i].repeats * data[i].solved_sets;
+
         nameCell.appendChild(document.createTextNode(data[i].name));
-        weightCell.appendChild(document.createTextNode(data[i].weight));
-        repsCell.appendChild(document.createTextNode(data[i].repeats));
         setsCell.appendChild(document.createTextNode(data[i].solved_sets));
+        repsCell.appendChild(document.createTextNode(data[i].repeats));
+        weightCell.appendChild(document.createTextNode(data[i].weight));
+        totalWeightCell.appendChild(document.createTextNode(totalWeight));
         muscleCell.appendChild(document.createTextNode(data[i].musclegroup));
+
         row.appendChild(nameCell);
-        row.appendChild(weightCell);
-        row.appendChild(repsCell);
         row.appendChild(setsCell);
+        row.appendChild(repsCell);
+        row.appendChild(weightCell);
+        row.appendChild(totalWeightCell);
         row.appendChild(muscleCell);
         table.appendChild(row);
     }
+
     if (only_exercise) {
         const row = document.createElement("tr");
         const nameCell = document.createElement("td");
-        const weightCell = document.createElement("td");
-        const repsCell = document.createElement("td");
         const setsCell = document.createElement("td");
+        const repsCell = document.createElement("td");
+        const weightCell = document.createElement("td");
+        const totalWeightCell = document.createElement("td");
         const muscleCell = document.createElement("td");
+        const totalWeight = data.weight * data.repeats * data.solved_sets;
+
         nameCell.appendChild(document.createTextNode(data.name));
-        weightCell.appendChild(document.createTextNode(data.weight));
-        repsCell.appendChild(document.createTextNode(data.repeats));
         setsCell.appendChild(document.createTextNode(data.solved_sets));
+        repsCell.appendChild(document.createTextNode(data.repeats));
+        weightCell.appendChild(document.createTextNode(data.weight));
+        totalWeightCell.appendChild(document.createTextNode(totalWeight));
         muscleCell.appendChild(document.createTextNode(data.musclegroup));
+
         row.appendChild(nameCell);
-        row.appendChild(weightCell);
-        row.appendChild(repsCell);
         row.appendChild(setsCell);
+        row.appendChild(repsCell);
+        row.appendChild(weightCell);
+        row.appendChild(totalWeightCell);
         row.appendChild(muscleCell);
         table.appendChild(row);
     }
+
     const container = document.createElement("div");
     const heading = document.createElement("h3");
     heading.appendChild(document.createTextNode(title));
