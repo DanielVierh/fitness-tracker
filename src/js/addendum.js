e3e41427ac7b14c91d90ccd218ecc7df678cbccc
Dan <daniel.vierheilig89@yahoo.de>
2025-08-04 16:52:28 +0200

    more tests

--- src/js/addendum.js
+++ src/js/addendum.js
@@ -8,13 +8,13 @@ export async function addendum(save_Object) {
         e.preventDefault();
     
         const trainingsname = inp_add_training_name.value;
-        const trainings_date = inp_add_training_date.value;
-        const trainingsduration = inp_add_training_time.value;
-        const trainings_place = training_place.value;
+        const addendum_trainings_date = inp_add_training_date.value;
+        const addendum_trainingsduration = inp_add_training_time.value;
+        const addendum_trainings_place = training_place.value;
 
-        const day = splitVal(trainings_date, '-',2);
-        const month = splitVal(trainings_date, '-',1);
-        const year = splitVal(trainings_date, '-',0);
+        const day = splitVal(addendum_trainings_date, '-',2);
+        const month = splitVal(addendum_trainings_date, '-',1);
+        const year = splitVal(addendum_trainings_date, '-',0);
         const date = `${day}.${month}.${year}`;
 
         console.log('date', date);
@@ -22,14 +22,13 @@ export async function addendum(save_Object) {
         
         
         for(let i = 0; save_Object.trainings.length; i++) {
-            console.info('save_Object.trainings[i].training_date', save_Object.trainings[i].training_date);
-            
-            if(save_Object.trainings[i].training_date === date) {
+            const i_date = save_Object.trainings[i].training_date;
+            if(i_date === date) {
                 console.info('Gleicher Tag')
             }
         }
     
-        console.log(`trainingsname: ${trainingsname} trainings_date: ${trainings_date}  trainingsduration: ${trainingsduration} trainings_place ${trainings_place}..       `);
+        console.log(`trainingsname: ${trainingsname} trainings_date: ${date}  trainingsduration: ${addendum_trainingsduration} trainings_place ${addendum_trainings_place}..       `);
     })
     
 }
