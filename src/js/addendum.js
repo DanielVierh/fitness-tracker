020282832b5f9ee0cdda508901fb29bf38ce05c6
Dan <daniel.vierheilig89@yahoo.de>
2025-08-04 17:05:05 +0200

    Add func to save

--- src/js/addendum.js
+++ src/js/addendum.js
@@ -20,13 +20,26 @@ export async function addendum(save_Object) {
         console.log('date', date);
         console.log('trainings length',save_Object.trainings.length);
         
-        
+        //* Loop through trainings to detect date
         for(let i = 0; i < save_Object.trainings.length; i++) {
             const i_date = save_Object.trainings[i].training_date;
             console.log('i_date', i_date);
             
-            if(i_date === date) {
-                console.info('Gleicher Tag');
+            if(i_date <= date) {
+                console.info('Kleiner gleich');
+                const new_index = i - 1;
+                console.log('new_index', new_index);
+                save_Object.trainings.splice(new_index, 0, {
+                    training_name: addendum_trainingsname,
+                    training_date: date,
+                    training_duration: addendum_trainingsduration,
+                    training_place: addendum_trainings_place,
+                    training_id: save_Object.trainings.length + 1
+                });
+                console.log('save_Object.trainings', save_Object.trainings);
+                localStorage.setItem('stored_fitness_saveobj', JSON.stringify(save_Object));
+                break;
+
             }
         }
     
